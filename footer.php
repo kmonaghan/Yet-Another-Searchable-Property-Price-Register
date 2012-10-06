b89f039465ddd6ac27e6386e34931bced1489b1a
Karl Monaghan <karl.t.monaghan@gmail.com>
2012-10-06 20:07:31 +0100

    header and footer files

---
+++ footer.php
@@ -0,0 +1,35 @@
+            <hr>
+
+            <footer>
+                <p>By <a href="http://www.karlmonaghan.com/">Karl Monaghan</a> &amp; <a href="https://twitter.com/jymian">Mike McHugh</a>&nbsp;|&nbsp;Data provided by <a href="http://propertypriceregister.ie">Residential Property Price Register</a>&nbsp;|&nbsp;<a href="http://www.karlmonaghan.com/contact">Get in touch</a></p>
+            </footer>
+
+        </div> <!-- /container -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+		
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.1.min.js"><\/script>')</script>
+
+		<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/vendor/bootstrap-datepicker.js"></script>
+        <script src="js/main.js?v=2"></script>
+
+        <script>  
+            var _gaq=[['_setAccount','UA-5653857-4'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+<?php
+if (isset($results['results']))
+{
+?>
+			results = <?php echo json_encode($results['results']); ?>;
+<?php
+}
+?>
+
+        </script>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Yet Another Searchable Property Price Register</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/datepicker.css">
+        <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.1.0.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+    	<center>
+			<script type="text/javascript"><!--
+				google_ad_client = "ca-pub-1189639444988756";
+				/* karlmonaghan.com leaderboard */
+				google_ad_slot = "3806467395";
+				google_ad_width = 728;
+				google_ad_height = 90;
+			//-->
+			</script>
+				<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+			</script>
+    	</center>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <!-- This code is taken from http://twitter.github.com/bootstrap/examples/hero.html -->
+
+		<header id="overview" class="jumbotron subhead">
+			<div class="container">
+				<h1><a href="index.php">Yet Another Searchable Property Price Register</a></h1>
+				<p class="lead">A mapped version of the Property Price Register.</p>
+			</div>
+		</header>
+		
+		<div class="container-fluid">        	
